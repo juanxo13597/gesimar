2bcacd4b61a2e94dacb7d23f0e40e02d0dfa6958
juanxo13597 <juanxo97master@gmail.com>
2020-03-29 13:08:55 +0200

    preparando controller home para sacar informacion

--- controllers/home.php
+++ controllers/home.php
@@ -1,4 +1,11 @@
 <?php
+require_once("models/clientes.php");
+require_once("models/instalacion.php");
+require_once("models/incidencia.php");
+$cliente = new cliente;
+$instalacion = new instalacion;
+$incidencia = new incidencia;
+
 
 require_once("views/home.php");
 ?>
